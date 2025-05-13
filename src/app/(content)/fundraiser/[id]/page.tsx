0aa580747b74131ffd3864753f7ff4d1691fe794
jet <88599825+jjeett0007@users.noreply.github.com>
2025-05-13 20:48:16 +0000

    update

--- src/app/(content)/fundraiser/[id]/page.tsx
+++ src/app/(content)/fundraiser/[id]/page.tsx
@@ -36,8 +36,61 @@ import type { FundraiserData, Donor } from "@/utils/type";
 import { motion } from "framer-motion";
 import UserInfoDialog from "./components/UserInfoDialog";
 import SuccessDialog from "./components/SuccessDialog";
+import type { Metadata } from "next";
 
 // User information dialog component
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const response = await apiRequest(
+    "GET",
+    `/fundraise/get-fundraise/${params.id}`
+  );
+
+  if (!response.success) {
+    return {
+      title: "Emerg Funds",
+      description:
+        "Emerg Funds Raising is a dedicated platform designed to help individuals and organizations quickly raise emergency funds for urgent needs. Easily create, share, and support fundraising campaigns for medical emergencies, disaster relief, and other critical situations.",
+    };
+  }
+
+  const responseData = response.data;
+
+  return {
+    title: `${responseData.fundMetaData.title} | Emerg Funds`,
+    description: responseData.fundMetaData.description,
+    icons: {
+      icon: [{ url: responseData.fundMetaData.imageUrl }],
+      apple: [{ url: responseData.fundMetaData.imageUrl }],
+      other: [
+        { rel: "mask-icon", url: responseData.fundMetaData.imageUrl }
+      ]
+    },
+    openGraph: {
+      title: responseData.fundMetaData.title,
+      description: responseData.fundMetaData.description,
+      images: responseData.fundMetaData
+        ? [{ url: responseData.fundMetaData.imageUrl }]
+        : []
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: responseData.fundMetaData.title,
+      description: responseData.fundMetaData.description,
+      images: responseData.fundMetaData
+        ? [responseData.fundMetaData.imageUrl]
+        : []
+    }
+  };
+}
+
 
 export default function FundraiserPage() {
   const router = useRouter();
